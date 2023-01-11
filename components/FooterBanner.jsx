15befa39f892842ed6fe9043a021cb918020eb47
santhosh <santhosh29012001@gmail.com>
2023-01-11 07:01:55 -0800

    first commit

---
+++ components/FooterBanner.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Link from 'next/link';
+
+import { urlFor } from '../lib/client';
+
+const FooterBanner = ({ footerBanner: { discount, largeText1, largeText2, saleTime, smallText, midText, desc, product, buttonText, image } }) => {
+  return (
+    <div className="footer-banner-container">
+  
+        <div className="left">
+
+          <h3>{largeText1}</h3>
+          <h3>{largeText2}</h3>
+          <p>{desc}</p>
+   
+          <Link href="#product">
+            <button type="button">{buttonText}</button>
+          </Link>
+        </div>
+   
+<div className='footer-banner-image'>
+        <img 
+          src={
+            urlFor(image)
+          } 
+        />
+        </div>
+      </div>
+
+  )
+}
+
+export default FooterBanner
